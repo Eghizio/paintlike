03b0e4feb3ffe9ab9a3c78e6074f3fd02d203e5d
Jakub Wąsik <32049761+Eghizio@users.noreply.github.com>
2024-10-09 02:27:44 +0200

    Upload downloaded canvas

--- src/main.ts
+++ src/main.ts
@@ -17,29 +17,83 @@ const getCanvasContextSafely = (
 
 const getColor = (): Color => getElementSafely("input").value;
 
-const downloadCanvasData = (canvas: HTMLCanvasElement) => {
-  const context = getCanvasContextSafely(canvas);
-  const data = context.getImageData(0, 0, canvas.width, canvas.height);
-
-  const timestamp = Date.now();
-  const id = Math.ceil(Math.random() * 1_000_000).toString(16);
-  const fileName = `canvas_data_${timestamp}_${id}.json`;
-
-  const fileToSave = new Blob([JSON.stringify(data)], {
+const download = (fileName: string, serializedData: string) => {
+  const fileToSave = new Blob([serializedData], {
     type: "application/json",
     // type: "octet/stream",
   });
 
+  const url = window.URL.createObjectURL(fileToSave);
+
   const a = document.createElement("a");
   a.setAttribute("style", "display: none;");
-  document.body.appendChild(a);
-  const url = window.URL.createObjectURL(fileToSave);
   a.href = url;
   a.download = fileName;
+  document.body.appendChild(a);
+
   a.click();
+
   window.URL.revokeObjectURL(url);
   a.remove();
 };
+
+const loadCanvasData = (file: File) => {
+  const reader = new FileReader();
+  reader.addEventListener("load", (event) => {
+    if (!event.target) return;
+    const data = event.target.result;
+
+    if (!data) return;
+
+    try {
+      const canvasData = JSON.parse(data.toString());
+
+      const imgData = new ImageData(canvasData.width, canvasData.height, {
+        colorSpace: canvasData.colorSpace,
+      });
+      imgData.data.set(canvasData.data);
+
+      ctx.putImageData(imgData, 0, 0);
+    } catch (error) {
+      console.log(error);
+    }
+  });
+
+  reader.readAsText(file);
+};
+
+const downloadCanvasData = (canvas: HTMLCanvasElement) => {
+  const timestamp = Date.now();
+  const id = Math.ceil(Math.random() * 1_000_000).toString(16);
+  const fileName = `canvas_data_${timestamp}_${id}.json`;
+
+  const context = getCanvasContextSafely(canvas);
+  const { data, colorSpace, width, height } = context.getImageData(
+    0,
+    0,
+    canvas.width,
+    canvas.height
+  );
+
+  const payload = {
+    data: [...data],
+    colorSpace: colorSpace.toString(),
+    width: Number(width),
+    height: Number(height),
+  };
+
+  const serializedPayload = JSON.stringify(payload);
+
+  download(fileName, serializedPayload);
+};
+
+document.querySelector("input#upload")?.addEventListener("change", (event) => {
+  if (!(event.target instanceof HTMLInputElement)) return;
+
+  const [file] = [...(event.target.files ?? [])];
+
+  loadCanvasData(file);
+});
 
 const canvas = getElementSafely("canvas");
 const ctx = getCanvasContextSafely(canvas);
